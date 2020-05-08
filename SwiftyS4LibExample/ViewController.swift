39d6603997299303324e327806c5155e5f2396b4
Nguyen Phuc Minh <minh.np@workhouse.me>
2020-05-08 13:31:32 +0700

    init code

---
+++ SwiftyS4LibExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyS4LibExample
+//
+//  Created by Admin on 5/8/20.
+//  Copyright © 2020 NPM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
