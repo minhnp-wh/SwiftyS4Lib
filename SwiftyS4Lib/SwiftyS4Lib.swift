38d77488adce96f314a4a99de82a2858935a591f
Nguyen Phuc Minh <minh.np@workhouse.me>
2020-05-08 14:22:26 +0700

    Added SwiftyS4Lib.podspec

---
+++ SwiftyS4Lib/SwiftyS4Lib.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftyS4Lib.swift
+//  SwiftyS4Lib
+//
+//  Created by Admin on 5/8/20.
+//  Copyright © 2020 NPM. All rights reserved.
+//
+
+import Foundation
+
+public final class SwiftyS4Lib {
+
+    let name = "SwiftyS4Lib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
