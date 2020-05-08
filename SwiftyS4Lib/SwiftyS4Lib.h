12fdef8c8982c2f827b3fdd7a5d2a8c42b295448
Nguyen Phuc Minh <minh.np@workhouse.me>
2020-05-08 11:50:25 +0700

    Initial Commit

---
+++ SwiftyS4Lib/SwiftyS4Lib.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyS4Lib.h
+//  SwiftyS4Lib
+//
+//  Created by Admin on 5/8/20.
+//  Copyright © 2020 NPM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyS4Lib.
+FOUNDATION_EXPORT double SwiftyS4LibVersionNumber;
+
+//! Project version string for SwiftyS4Lib.
+FOUNDATION_EXPORT const unsigned char SwiftyS4LibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyS4Lib/PublicHeader.h>
+
+
